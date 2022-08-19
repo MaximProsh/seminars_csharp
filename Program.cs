f8e8ccd8b99107735bcbefa0f769b7c7c10b8c54
MaximProsh <karmark@yandex.ru>
2022-08-19 14:12:06 +0300

    Added homework

--- Program.cs
+++ Program.cs
@@ -1,12 +1,75 @@
-﻿int num = Convert.ToInt32(Console.ReadLine());
-int count = -num;
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+int fnum = Convert.ToInt32(Console.ReadLine());
+int snum = Convert.ToInt32(Console.ReadLine());
+if (fnum > snum)
+{
+    Console.Write("max: ");
+    Console.WriteLine(fnum);
+}
+else
+{
+    Console.Write("max: ");
+    Console.WriteLine(snum);
+}
+
+// Задача 4: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+int fnum = Convert.ToInt32(Console.ReadLine());
+int snum = Convert.ToInt32(Console.ReadLine());
+int tnum = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (fnum > snum)
+{
+    max = fnum;
+}
+else
+{
+    max = snum;
+}
+if (tnum > max)
+{
+    max = tnum;
+}
+Console.WriteLine(max);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
 while (count != num + 1)
 {
     Console.Write(count);
     Console.Write(" ");
-    count +=1;
+    count += 1;
 }
-Console.WriteLine();
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num % 10;
+// Console.WriteLine(result);
+
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = -num;
+// while (count != num + 1)
+// {
+//     Console.Write(count);
+//     Console.Write(" ");
+//     count +=1;
+// }
+// Console.WriteLine();
 
 // int num = Convert.ToInt32(Console.ReadLine());
 // if(num == 1)
