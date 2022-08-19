d7607be74bee9fc33a2c711dc6d6a6f55aa07e95
MaximProsh <karmark@yandex.ru>
2022-08-19 12:57:16 +0300

    Added days of week and cicle

--- Program.cs
+++ Program.cs
@@ -1,3 +1,62 @@
-﻿int number = Convert.ToInt32(Console.ReadLine());
-int result = number * number;
-Console.WriteLine(result);
+﻿int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while (count != num + 1)
+{
+    Console.Write(count);
+    Console.Write(" ");
+    count +=1;
+}
+Console.WriteLine();
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+
+// if(num == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+
+// if(num == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+
+// if(num == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+
+// if(num == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+
+// if(num == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+
+// if(num == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+
+// int fnum = Convert.ToInt32(Console.ReadLine());
+// int snum = Convert.ToInt32(Console.ReadLine());
+
+// if (snum * snum == fnum)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result = number * number;
+// Console.WriteLine(result);
